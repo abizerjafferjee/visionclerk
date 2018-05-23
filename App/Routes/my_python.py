c58a1b31f153ee976ce876311e8e360b32a0603a
abizerjafferjee <abizerjafferjee@gmail.com>
2018-05-23 15:59:29 -0400

    remove from app routes

--- App/Routes/my_python.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    app_string = sys.argv
-    #print('python program processing this user query: ' + str(app_string[1]))
-    print('vc_documents')
-    sys.stdout.flush()
