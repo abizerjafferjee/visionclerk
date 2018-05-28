3a05df5d8466ccebbdba028ddae08b03b6b91b58
abizerjafferjee <abizerjafferjee@gmail.com>
2018-05-28 13:28:58 -0400

    adding contract model

---
+++ App/Models/contract.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var validate = require('mongoose-validator');
+var bcrypt   = require('bcrypt-nodejs');
+
+
+var contractSchema = new mongoose.Schema({
+  organization: {type: String},
+  party: {type: String},
+  identifier: {type: String},
+  item: {type: String},
+  quantity: {type: String},
+  event: {type: String},
+  other: {type: String},
+  originalFile: {
+    fileName: {type: String},
+    fileRef: {type: mongoose.Schema.Types.ObjectId, ref:"File"}
+  },
+  user: {type: mongoose.Schema.Types.ObjectId, ref:"User"}
+});
+
+module.exports = mongoose.model("Contract", contractSchema);
